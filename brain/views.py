42ad86fb88d352f099b2a7c088a677aad60f0c9e
Eddy O'Donnell <eddyod@yahoo.com>
2020-03-03 08:07:43 -0800

    Added slides, scans, tiffs to list.html

--- brain/views.py
+++ brain/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from brain.models import Animal, Slide, SlideCziToTif
+from brain.models import Animal, ScanRun, Slide, SlideCziToTif
 from brain.forms import AnimalForm
 
 # from url initial page
@@ -9,8 +9,17 @@ def image_list(request):
         prep_id = 'DK43'
     
     form = AnimalForm()  # A form bound to the GET data
-    datarows = Animal.objects.filter(prep_id=prep_id).order_by('prep_id')
-    prep_id = Animal.objects.get(prep_id=prep_id)
+    animals = Animal.objects.filter(prep_id=prep_id).order_by('prep_id')
+    animal = Animal.objects.get(prep_id=prep_id)
+    scans = ScanRun.objects.filter(prep_id=prep_id).order_by('created')
+    slides = Slide.objects.filter(scan_run_id__in=[1,2]).order_by('file_name')
+    tiffs = SlideCziToTif.objects.filter(slide_id__in=[1,2]).order_by('file_name')
 
-    return render(request, 'list.html',{'datarows': datarows, 'form':form, 'animal_title':prep_id})
+    return render(request, 'list.html',{'animals': animals,
+                                        'animal': animal, 
+                                        'scans': scans, 
+                                        'slides': slides, 
+                                        'tiffs': tiffs,
+                                        'form': form, 
+                                        'animal_title':prep_id})
 
