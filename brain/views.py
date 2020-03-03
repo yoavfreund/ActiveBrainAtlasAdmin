64c65a0e4b305e7b55bfedad8c174b1c25e72e51
Eddy O'Donnell <eddyod@yahoo.com>
2020-03-03 07:35:11 -0800

    Added form and bootstrap

--- brain/views.py
+++ brain/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from brain.models import Animal, Slide, SlideCziToTif
+from brain.forms import AnimalForm
 
-# Create your views here.
+# from url initial page
+def image_list(request):
+    prep_id = request.GET.get('prep_id')
+    if not prep_id:
+        prep_id = 'DK43'
+    
+    form = AnimalForm()  # A form bound to the GET data
+    datarows = Animal.objects.filter(prep_id=prep_id).order_by('prep_id')
+    prep_id = Animal.objects.get(prep_id=prep_id)
+
+    return render(request, 'list.html',{'datarows': datarows, 'form':form, 'animal_title':prep_id})
+
