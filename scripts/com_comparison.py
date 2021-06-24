5f4e3c891144845319adabf3992bc7b208440555
Edward O'Donnell <eddyod@yahoo.com>
2021-06-24 10:53:38 -0700

    Added more methods to atlas.py

--- scripts/com_comparison.py
+++ scripts/com_comparison.py
@@ -31,6 +31,7 @@ def prepare_table_for_save(brains, common_structures, person_id, input_type_id):
                   for s in common_structures]
         offset = np.array(offset)
         scale = np.array([10, 10, 20])
+        scale = np.array([1,1,1])
         dx, dy, dz = (offset * scale).T
         dist = np.sqrt(dx * dx + dy * dy + dz * dz)
         for dx_i, dy_i, dz_i, dist_i in zip(dx, dy, dz, dist):
@@ -43,7 +44,7 @@ def test_com():
       .filter(input_type__input_type__in=['manual'])\
       .filter(layer='COM')\
       .filter(active=True)\
-      .exclude(prep_id='Atlas')\
+      .exclude(prep_id__in=['Atlas','MD589','DK46'])\
       .values_list('prep_id', flat=True).distinct().order_by('prep_id'))
   print(brains)
   atlas_coms = get_atlas_centers()
